6387a874a212a47b4616519ea5ba984973107060
Martin Englund <englund@fb.com>
2014-09-18 09:57:43 -0700

    make the run script try to pull down serf if it can't find it

--- run.sh
+++ run.sh
@@ -1,12 +1,41 @@
 #!/bin/sh
 
-URL="http://www.serfdom.io/downloads.html"
+OS_NAME=`uname -s`
+BASE=`dirname $0`
 
-SERF=`which serf`
-if [ -z "$SERF" ]; then
-	echo "You don't have serf installed, go to $URL and download it..."
+if [ $OS_NAME != "Darwin" ]; then
+	echo "chorus only runs on MacOS"
 	exit 1
 fi
 
-BASE=`dirname $0`
-serf agent -discover=chorus -event-handler user:say=$BASE/chorus.sh
+SERF=`which serf`
+if [ -z "$SERF" ]; then
+	echo "You don't have serf installed, I'll try to fetch it for you..."
+	
+	WGET=`which wget`
+	if [ -z "$WGET" ]; then
+		echo "You don't have wget installed, I'll try to fetch it for you..."
+		
+		BREW=`which brew`
+		if [ -z "$BREW" ]; then
+			echo "You don't have home brew installed, what is wrong with you?"
+			echo "I give up! You have to either install serf by hand or home brew:"
+			echo "\thttp://www.serfdom.io/downloads.html"
+			echo "\thttp://brew.sh/"
+			exit 1
+		fi
+		
+		$BREW install wget
+		WGET=`which wget`
+		if [ -z "$WGET" ]; then
+			echo "wget installation failed!"
+			exit 1
+		fi
+	fi
+	
+	$WGET -q https://dl.bintray.com/mitchellh/serf/0.6.3_darwin_amd64.zip
+	unzip 0.6.3_darwin_amd64.zip
+	SERF="$BASE/serf"
+fi
+
+exec $SERF agent -discover=chorus -event-handler user:say=$BASE/chorus.sh
